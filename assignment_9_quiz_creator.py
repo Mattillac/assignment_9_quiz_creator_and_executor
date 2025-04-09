1248d22c7ecfb43ef16e2cdd7da4208f3afde253
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-04-09 15:03:36 +0800

    here is now the final touches of my program, as you can see that i added a loop so the user can have an easy way to continue the program without reexeceuting it, and as i program it i tested it and as you can see i found an error on my work on it was the file_reader.close as i added it on the bottom of my print, thus making it close immediately and cause error now that i added it on the bottom it now works freely, and also one thing i found is that the continous typing of the file name in order to proceed again, so i fixed it and moved the line 4&5 to the top of the loop that i forgot the python is a tab and case sensitive language. Now my program is fully working as you can see with the help of Sir Dan's prev work and recent batches. You can clearly see it i used it on my work as the .upper and loops from batch 1-3

--- assignment_9_quiz_creator.py
+++ assignment_9_quiz_creator.py
@@ -3,14 +3,24 @@
 #enter quiz name:
 quiz_name = input("Enter the quiz name: ")
 file_reader = open(quiz_name, "w")
-#enter a question:
-question_maker = input("Enter a question: ")
-answer_sheet = input("Enter the answer sheet like this (a, b, c, d (a) ) and enclose with parenthesis the correct answer!: ")
-#asks for the possible four answers for ex. (a, b, c, d) and it must include the correct answer
-#enter the correct answer:
-file_reader.write(question_maker + "\n")
-file_reader.write(answer_sheet + "\n\n")
-file_reader.close()
-#it must write the collected data and convert it into a .txt file, and must ask continuously until the user says no/exit
-#create more? yes/no
+while True:
+    #enter a question:
+    question_maker = input("Enter a question: ")
+    answer_sheet = input("Enter the answer sheet like this (a, b, c, d (a) ) and enclose with parenthesis the correct answer!: ")
+    #asks for the possible four answers for ex. (a, b, c, d) and it must include the correct answer
+    #enter the correct answer:
+    file_reader.write(question_maker + "\n")
+    file_reader.write(answer_sheet + "\n\n")
 
+    program_buster = input("Do you want to add more questions? [YES/NO]: ").upper()
+    #it must write the collected data and convert it into a .txt file, and must ask continuously until the user says no/exit
+    if program_buster == "YES":
+        continue
+    elif program_buster == "NO":
+        break
+    else:
+        print("SHAME ON YOU WHY YOU DIDN'T FOLLOWED THE INSTRUCTIONS")
+        break
+    #create more? yes/no
+file_reader.close()
+print("file is saved! thank you for using this program")
