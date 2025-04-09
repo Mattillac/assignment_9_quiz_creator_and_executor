2b9ad5082575f01c31cd05ffead2349fc8ffbcbb
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-04-09 15:52:07 +0800

    another thing to add im so sorry i just noticed that my final output does not end in a txt format soo making it just a file which can be opened in many way

--- assignment_9_quiz_creator.py
+++ assignment_9_quiz_creator.py
@@ -3,6 +3,7 @@
 #enter quiz name:
 quiz_name = input("Enter the quiz name: ")
 file_reader = open(quiz_name, "a")
+    quiz_name += ".txt"
 while True:
     #enter a question:
     question_maker = input("Enter a question: ")
