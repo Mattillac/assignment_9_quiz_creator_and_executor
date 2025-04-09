b29564f8cc6238f4fcde3cd517bf4d2f19216000
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-04-09 14:42:36 +0800

    here is my first half of my program based on my trial awhile ago as you can see in the video, but in here i used the example given on the ppt about file reading. i used a so it doesn't overwrite the data cause sometimes overwriting data can complicate further things such as corruptions. and i added the input things since it is required to input a question and answer sheets. Overall i based my work on Sir Dan's ppt and with the use of my past knowledge about Sir Dan's batches and assignments.

--- assignment_9_quiz_creator.py
+++ assignment_9_quiz_creator.py
@@ -1,9 +1,16 @@
 #TITLE:assignment_9_quiz_creator
 #asks the user to input a quiz name and number of questions
 #enter quiz name:
+quiz_name = input("Enter the quiz name: ")
+file_reader = open(quiz_name, "w")
 #enter a question:
+question_maker = input("Enter a question: ")
+answer_sheet = input("Enter the answer sheet like this (a, b, c, d (a) ) and enclose with parenthesis the correct answer!: ")
 #asks for the possible four answers for ex. (a, b, c, d) and it must include the correct answer
 #enter the correct answer:
+file_reader.write(question_maker + "\n")
+file_reader.write(answer_sheet + "\n\n")
+file_reader.close()
 #it must write the collected data and convert it into a .txt file, and must ask continuously until the user says no/exit
 #create more? yes/no
 
