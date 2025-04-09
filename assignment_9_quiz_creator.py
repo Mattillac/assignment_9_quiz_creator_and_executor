02fc72fea1add9d3261315b4f700f03d2d7d913b
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-04-09 13:42:28 +0800

    here is my recommit as you'll see why on the video, i encountered my problem again, just like the past project i didn't saved it before committing and pushing.

---
+++ assignment_9_quiz_creator.py
@@ -0,0 +1,9 @@
+#TITLE:assignment_9_quiz_creator
+#asks the user to input a quiz name and number of questions
+#enter quiz name:
+#enter a question:
+#asks for the possible four answers for ex. (a, b, c, d) and it must include the correct answer
+#enter the correct answer:
+#it must write the collected data and convert it into a .txt file, and must ask continuously until the user says no/exit
+#create more? yes/no
+
