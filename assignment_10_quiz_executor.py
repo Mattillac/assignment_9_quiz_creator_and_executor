d21682c58b87e0370b67dddb8307e8a802f2efd1
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-04-30 14:51:49 +0800

    here is my pseudocode for the quiz executor

---
+++ assignment_10_quiz_executor.py
@@ -0,0 +1,8 @@
+#QUIZ EXECUTOR
+#Enter the file name: 
+#Opens the file and reads the questions and answers
+#Prints the question (Who is the pirate in pirates of the carribean with a bird name?)
+# prints the possible answers (1. Jack Sparrow, 2. Captain Hook, 3. Davy Jones, 4. Blackbeard)
+#User will answer the question and the program will check if the answer is correct or not. 
+#If wrong kindly state the the user is wrong, but if correct, kindly state the user is correct.
+#asks the user if he wants to continue to answer more quizzes or not.
