6c5d2870dd5715611ae15bd999c022ba36c26756
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-04-30 16:44:42 +0800

    here as you can see that i defined the quiz_loader i added on my def main_engine(): this will act as my file loader in order to read later the files.

--- assignment_10_quiz_executor.py
+++ assignment_10_quiz_executor.py
@@ -1,5 +1,16 @@
 #QUIZ EXECUTOR
 import random
+
+#Opens the file and reads the questions and answers
+def quiz_loader_data(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            lines = [line.strip() for line in file.readlines() if line.strip()]
+        return lines
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found.")
+        return None
+    
  #Enter the file name: 
  def main_engine():
     print("HEY! WELCOME TO THE QUIZ EXECUTOR")
@@ -12,7 +23,7 @@ import random
 if __name__ == "__main__":
     main_engine()
 
- #Opens the file and reads the questions and answers
+ 
  #Prints the question (Who is the pirate in pirates of the carribean with a bird name?)
  # prints the possible answers (1. Jack Sparrow, 2. Captain Hook, 3. Davy Jones, 4. Blackbeard)
  #User will answer the question and the program will check if the answer is correct or not. 
